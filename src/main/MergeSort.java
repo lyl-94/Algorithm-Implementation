3cb9c2a103402c62f516d8bcc68af02a300c4e74
linyuliang <linyuliang@jd.com>
2018-10-09 10:55:17 +0800

    递归实现归并排序

--- src/main/MergeSort.java
+++ src/main/MergeSort.java
@@ -57,13 +57,39 @@ public class MergeSort {
 //递归实现
 class MergeSort递归实现{
 
-    public static void mergeSort(List<Integer> array){
+    public static void merge(int[] array, int start, int end){
+        int[] tmp = new int[end-start + 1];
+        int mid = start + (end-start)/2;
+        int i = start, j = mid + 1, point = 0;
+        while(true){
+            boolean left = i <= mid && i <= end;
+            boolean right = j <= end;
+            if (!right && left || (left && array[i] < array[j])){
+                tmp[point++] = array[i++];
+            }else if (right){
+                tmp[point++] = array[j++];
+            }else {
+                break;
+            }
+        }
+        for (int k = 0; k <= end-start; k++){
+            array[start+k] = tmp[k];
+        }
+    }
 
+    public static void sort(int[] array, int start, int end){
+        if (start >= end){
+            return;
+        }
+        int mid = start+(end-start)/2;
+        sort(array, start, mid);
+        sort(array, mid+1, end);
+        merge(array, start, end);
     }
 
     public static void main(String[] args){
-        List<Integer> array = Arrays.asList(5,1,7,3,4,6,8,2,3);
-        mergeSort(array);
-        System.out.println(array);
+        int[] array = {5,1,7,3,4,6,8,2,3,0,9,8,7};
+        sort(array, 0, array.length-1);
+        System.out.println(Arrays.toString(array));
     }
 }
